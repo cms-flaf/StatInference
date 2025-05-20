6053a9f3408d4537974f9045e717d59d850fc4a7
Devin Aebi <45836403+aebid@users.noreply.github.com>
2025-05-20 14:19:32 +0200

    Update Limit Setting (#8)
    * Updated config to use shape uncs and optimize to check negative bins on samples
    
    * Moved example call config to the analysis repo -- eventually will delete the StatInference config area
    
    * Deleted bbww config, now it is in hh_bbWW repo
    
    * Added res2b category to call example

--- bin_opt/call_optimize_binning.sh
+++ bin_opt/call_optimize_binning.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-cmbEnv python3 optimize_binning_bbWW.py --input-shapes /eos/user/d/daebi/bbWW_shape_files/ --output example_out/ --config ../config/x_hh_bbww_run3.yaml --mass-list 250,260,270,280,300,350,450,550,600,650,700,800
+python3 optimize_binning_bbWW.py --input-shapes /eos/user/d/daebi/bbWW/histograms/fwtest_15apr/Run3_2022EE/merged/ --file-name-pattern dnn_M0_Signal/dnn_M0_Signal.root --sig-name-pattern GluGlutoRadiontoHHto2B2Vto2B2L2Nu_M_{m} --output example_out/ --config ../../config/Datacards/x_hh_bbww_run3.yaml --mass-list 250,260,270,280,300,350,450,550,600,650,700,800 --channels eE,eMu,muMu --categories res1b_Zveto_MbbSR,res2b_Zveto_MbbSR --background-names TT,DY --important-backgrounds TT
--- bin_opt/optimize_binning_bbWW.py
+++ bin_opt/optimize_binning_bbWW.py
@@ -4,6 +4,15 @@ import uproot
 import json
 import sys
 import matplotlib.pyplot as plt
+from FLAF.RunKit.envToJson import get_cmsenv
+
+#I had to do this because now the ps_call for MergeResonantLimits wouldn't work in cmsEnv environment
+cmssw_env = get_cmsenv(cmssw_path=os.getenv("FLAF_CMSSW_BASE"))
+for var in [ 'HOME', 'ANALYSIS_PATH', 'ANALYSIS_DATA_PATH', 'X509_USER_PROXY', 'CENTRAL_STORAGE',
+                'ANALYSIS_BIG_DATA_PATH', 'FLAF_CMSSW_BASE', 'FLAF_CMSSW_ARCH' ]:
+    if var in os.environ:
+        cmssw_env[var] = os.environ[var]
+
 
 def integrate_uproot(hist, start, end):
     int_values = np.sum(hist.values()[start:end+1])
@@ -14,7 +23,7 @@ def convert_to_json(bins_dict, params):
     json_dict = []
     for key in bins_dict.keys():
         ch = key.split("_")[0]
-        cat = key.split("_")[1]
+        cat = "_".join(key.split("_")[1:])
         exists = False
         if not exists:
             entry = {
@@ -25,8 +34,8 @@ def convert_to_json(bins_dict, params):
             for key, value in params.items():
                 entry[key] = [ value ]
             json_dict.append(entry)
-
-
+                
+            
     print(json_dict)
     return json_dict
 
@@ -63,8 +72,8 @@ def optimize_binning(shapes_dir, filename, sig_string, params, mass_list, outdir
         hist_names = [ key.split(';')[0][len(ch_cat):] for key in file.keys() if key.startswith(ch_cat) ]
         hists = { hist_name : file[ch_cat+hist_name] for hist_name in hist_names }
 
-        print(f"Starting ch cat {ch_cat}")
-
+        print(f"Starting ch cat {ch_cat} with mass {mass_list}")
+        
         signal_name = sig_string #Maybe update to get from yaml later
 
         tmp_signal = np.sum(hists[signal_name].values())
@@ -89,14 +98,21 @@ def optimize_binning(shapes_dir, filename, sig_string, params, mass_list, outdir
                     integral_bkgs_unc = np.zeros(len(important_backgrounds))
                     tot_bkgs = 0
                     tot_bkgs_unc = 0
+                    negative_bins = False
                     for i, bkg_name in enumerate(bkg_names):
                         int_bkg = integrate_uproot(hists[bkg_name], left_edge, right_edge)
+                        if int_bkg[0] < 0.0:
+                            print(f"Background {bkg_name} has a negative bin, try again")
+                            negative_bins = True
                         if bkg_name in important_backgrounds: #Only check important backgrounds for the individual tests
                             integral_bkgs[i] = int_bkg[0]
                             integral_bkgs_unc[i] = (int_bkg[1])
 
                         tot_bkgs += int_bkg[0] #But check all backgrounds for the total tests
                         tot_bkgs_unc += (int_bkg[1]**(2.0))
+
+                    if negative_bins:
+                        continue
 
                     tot_bkgs_unc = np.sqrt(tot_bkgs_unc)
 
@@ -105,18 +121,20 @@ def optimize_binning(shapes_dir, filename, sig_string, params, mass_list, outdir
                         custom_binning.append(left_edge)
                         custom_totals.append(curr_integral_signal)
                         done = True
+                        print("Hit the left edge")
                         break
 
                     if (tot_integral_signal >= big_bin_counter*bin_content_goal):
                         print(f"Check out the background unc/tot {integral_bkgs_unc/integral_bkgs}")
                         print(f"Check the backgrounds for empty bins")
                         for bkg_name in bkg_names:
-                            print(f"{bkg_name} has {integrate_uproot(hists[bkg_name], left_edge, right_edge)[0]} entries")
+                            this_bkg_int = integrate_uproot(hists[bkg_name], left_edge, right_edge)
+                            print(f"{bkg_name} has {this_bkg_int[0]} entries and {this_bkg_int[1]} uncertainty")
 
                         print(f"And the total bkg / tot bkg unc is {tot_bkgs} / {tot_bkgs_unc} = {tot_bkgs/tot_bkgs_unc}")
 
 
-                    if (tot_integral_signal >= big_bin_counter*bin_content_goal) and (np.max(integral_bkgs_unc/integral_bkgs) <= 0.2) and (tot_bkgs_unc/tot_bkgs <= 0.2):
+                    if (tot_integral_signal >= big_bin_counter*bin_content_goal) and (np.max(integral_bkgs_unc/integral_bkgs) <= 0.2) and (tot_bkgs_unc/tot_bkgs <= 0.2):       
                         custom_binning.append(round(hists[signal_name].axis().edges()[left_edge], 2))
                         custom_totals.append(curr_integral_signal)
                         print("At bin ", left_edge, " integral is ", custom_totals[-1])
@@ -141,6 +159,7 @@ def optimize_binning(shapes_dir, filename, sig_string, params, mass_list, outdir
 
             json_dict = convert_to_json(bins_dict, params)
 
+
             with open(os.path.join(outdir, f'tmp_binning_{sig_string}.json'), 'w') as f:
                 json.dump(json_dict, f)
 
@@ -149,7 +168,8 @@ def optimize_binning(shapes_dir, filename, sig_string, params, mass_list, outdir
             datacards_dir = os.path.join(outdir, f'tmp_datacards_{sig_string}')
             binning_file = os.path.join(outdir, f'tmp_binning_{sig_string}.json')
 
-            ps_call(f"python3 ../dc_make/create_datacards.py --input {shapes_dir} --output {datacards_dir} --config {config} --hist-bins ./{binning_file} --param_values {mass_list}", shell=True)
+
+            ps_call(f"python3 ../dc_make/create_datacards.py --input {shapes_dir} --output {datacards_dir} --config {config} --hist-bins ./{binning_file} --param_values {mass_list}", shell=True, env=cmssw_env)
             #Find out where the limits will be saved
             output = ps_call(f"law run MergeResonantLimits --version {tag} --datacards '{datacards_dir}/*.txt' --print-output 0,False", shell=True, catch_stdout=True, split="\n")[1]
             limit_file_name = output[-2]
@@ -166,7 +186,7 @@ def optimize_binning(shapes_dir, filename, sig_string, params, mass_list, outdir
             limit_list.append(exp_limit)
 
             #If adding a bin does not improve (limit stays the same) then just move on
-            if (exp_limit < best_limit) or (nbins == 1):
+            if (exp_limit <= best_limit) or (nbins == 1):
                 best_limit = exp_limit
                 best_bins = bins_dict.copy()
 
@@ -211,11 +231,11 @@ if __name__ == '__main__':
         parser.add_argument('--mass-list', required=True, type=str, help="list of mass values to scan over")
         parser.add_argument('--nBinsMax', required=False, type=int, default=20, help="Maximum number of bins to test")
         parser.add_argument('--sig-name-pattern', required=False, type=str, default="ggRadion_HH_bbWW_M{}", help="Name format of signal in the shape files")
-        parser.add_argument('--file-name-pattern', required=False, type=str, default="Run3_2022/shape_m{}.root", help="Name format of input shape files")
-        parser.add_argument('--background-names', required=False, type=str, default="TT,DY,ST,VV", help="List of all background processes in the shapes file")
+        parser.add_argument('--file-name-pattern', required=False, type=str, default="Run3_2022/shape_m{m}.root", help="Name format of input shape files")
+        parser.add_argument('--background-names', required=False, type=str, default="TT", help="List of all background processes in the shapes file")
         parser.add_argument('--important-backgrounds', required=False, type=str, default="TT", help="List of background processes to consider for individual statistics")
         parser.add_argument('--categories', required=False, type=str, default="res2b,boost,res1b", help="List of jet categories")
-        parser.add_argument('--channels', required=False, type=str, default="mumu,emu,ee", help="List of lepton channels")
+        parser.add_argument('--channels', required=False, type=str, default="eE,eMu,muMu", help="List of lepton channels")
         parser.add_argument('--tag', required=False, type=str, default="dev", help="Name of combine job")
 
         args = parser.parse_args()
@@ -235,4 +255,4 @@ if __name__ == '__main__':
         tag = args.tag
 
         for mass in mass_list:
-            optimize_binning(shapes_dir, file_name_pattern.format(mass), signal_name_pattern.format(mass), {'MX': mass}, mass, outdir, config, nBinsMax, background_names, important_backgrounds, categories, channels, tag)
+            optimize_binning(shapes_dir, file_name_pattern.format(m = mass), signal_name_pattern.format(m = mass), {'MX': mass}, mass, outdir, config, nBinsMax, background_names, important_backgrounds, categories, channels, tag)
