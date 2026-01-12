feeb4d1ff2f55f7a58c5fea0f9f821f11ef6f88f
Towsifa Akhter <51368122+akhter-towsifa@users.noreply.github.com>
2026-01-12 07:31:42 -0600

    Binning Optimization with HTCondor workflow for the worker script (#13)
    * Rebasing commits from previous Binning optimization PR (#11)
    
    * testing multiple worker condor job
    
    * HTCondor job workflow

--- README.md
+++ README.md
@@ -1,23 +1,36 @@
 # StatInference
 
 ## How to run binning optimisation on lxplus
+Open two separate LXPLUS terminals: one for **server side** and the other for **worker side**.
+Set up the environment and proxy in analysis area as usual on both terminals.
+```sh
+source env.sh
+voms-proxy-init -voms cms -rfc -valid 192:00
+cd StatInference
+```
+Configure the `bin_opt/bin_optimization.yaml` file for the intended analysis (resonant or nonresonant), channels, era, etc.
 
+Get the server side script running first (it creates worker output directories) and takes about a few seconds. Then move on to the worker side terminal and run the worker side script there.
 ### Server side
-Better to run on screen.
+Better to run on screen. If screen does not work, you will need to have this server script below running on an active lxplus terminal.
 
 Example:
+For the current version of code, you will need to run the following script separately for each channel.
 ```sh
-source env.sh lcg
-python tools/optimize_channel.py --input /afs/cern.ch/user/f/fbrivio/public/datacards_5July2021_1000bins_DYmerged --channel tauTau_2018 --output output/binning_v2
+python3 bin_opt/optimize_channel.py --channel tauTau
 ```
 
 ### Worker side
 Running on screen is not required, jobs are submitted on HTCondor.
 
 Example:
+for HTCondor jobs, run the following:
 ```sh
-source env.sh hh
-python tools/submitLimitWorkers.py --output output/binning_v2 --channel tauTau_2018 --n-workers 8
+python3 bin_opt/submitLimitWorkers.py
+```
+The worker script can also be run locally as following:
+```sh
+python3 bin_opt/rebinAndRunLimitsWorker.py --channel tauTau
 ```
 
 If runtime is longer than one day, both server and workers should be restarted.
--- bin_opt/rebinAndRunLimitsWorker.py
+++ bin_opt/rebinAndRunLimitsWorker.py
@@ -11,47 +11,53 @@ with open(input_binning_opt_config, "r") as f:
 
 from rebinAndRunLimits import GetLimits
 
-for channel in input_binning_opt_config_dict["input"]["channels"]:
-    if os.path.isfile(os.path.join(input_binning_opt_config_dict["output"]["directory"],
-                                   f'{channel}_{str(input_binning_opt_config_dict["input"].get("era", ""))}.json')):
-                                   print( f"Results for channel {channel} already exist.")
-    else:
-        worker_dir = os.path.join(input_binning_opt_config_dict["output"]["directory"],
-        f'{channel}_{str(input_binning_opt_config_dict["input"].get("era", ""))}',
-                                            "workers",
-                                            uuid.uuid4().hex)
+from argparse import ArgumentParser
+parser = ArgumentParser()
+parser.add_argument('--channel', default="tauTau", type=str, help="one channel, e.g. tauTau, eMu")
+args = parser.parse_args()
+channel = args.channel
 
-        os.makedirs(worker_dir, exist_ok=True)
-        task_file = os.path.join(worker_dir, 'task.txt')
-        result_file = os.path.join(worker_dir, 'result.txt')
-        result_file_tmp = os.path.join(worker_dir, '.result.txt')
+#for channel in input_binning_opt_config_dict["input"]["channels"]:
+if os.path.isfile(os.path.join(input_binning_opt_config_dict["output"]["directory"],
+                                f'{channel}_{str(input_binning_opt_config_dict["input"].get("era", ""))}.json')):
+                                print( f"Results for channel {channel} already exist.")
+else:
+    worker_dir = os.path.join(input_binning_opt_config_dict["output"]["directory"],
+    f'{channel}_{str(input_binning_opt_config_dict["input"].get("era", ""))}',
+                                        "workers",
+                                        uuid.uuid4().hex)
 
-        verbose = input_binning_opt_config_dict.get("verbose", 1)
+    os.makedirs(worker_dir, exist_ok=True)
+    task_file = os.path.join(worker_dir, 'task.txt')
+    result_file = os.path.join(worker_dir, 'result.txt')
+    result_file_tmp = os.path.join(worker_dir, '.result.txt')
 
-        if verbose > 0:
-            print('Worker dir: {}'.format(worker_dir))
+    verbose = input_binning_opt_config_dict.get("verbose", 1)
 
-        while True:
-            time.sleep(1)
-            if os.path.isfile(task_file) and not os.path.isfile(result_file):
-                try:
-                    with open(task_file, 'r') as f:
-                        params = json.load(f)
-                except IOError:
-                    continue
-                bin_edges = params['bin_edges']
-                if verbose > 0:
-                    print('Bin edges: [ {} ]'.format(', '.join([ str(b) for b in bin_edges ])))
-                limit = GetLimits(params['input_datacard'], worker_dir, bin_edges, params['poi'], verbose=1,
-                                    other_datacards=params['other_datacards'])
+    if verbose > 0:
+        print('Worker dir: {}'.format(worker_dir))
 
-                if verbose > 0:
-                    print('Expected 95% CL limit = {}'.format(limit))
-                result = {
-                    'input_datacard': params['input_datacard'],
-                    'bin_edges': bin_edges,
-                    'exp_limit': limit
-                }
-                with open(result_file_tmp, 'w') as f:
-                    json.dump(result, f)
-                shutil.move(result_file_tmp, result_file)
+    while True:
+        time.sleep(1)
+        if os.path.isfile(task_file) and not os.path.isfile(result_file):
+            try:
+                with open(task_file, 'r') as f:
+                    params = json.load(f)
+            except IOError:
+                continue
+            bin_edges = params['bin_edges']
+            if verbose > 0:
+                print('Bin edges: [ {} ]'.format(', '.join([ str(b) for b in bin_edges ])))
+            limit = GetLimits(params['input_datacard'], worker_dir, bin_edges, params['poi'], verbose=1,
+                                other_datacards=params['other_datacards'])
+
+            if verbose > 0:
+                print('Expected 95% CL limit = {}'.format(limit))
+            result = {
+                'input_datacard': params['input_datacard'],
+                'bin_edges': bin_edges,
+                'exp_limit': limit
+            }
+            with open(result_file_tmp, 'w') as f:
+                json.dump(result, f)
+            shutil.move(result_file_tmp, result_file)
